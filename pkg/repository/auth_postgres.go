63eb9f9642a5c5c204eef44877d089329ad66be6
My Name <rbhb0555@mail.ru>
2024-12-27 16:45:14 +0300

    apdate code 3

--- pkg/repository/auth_postgres.go
+++ pkg/repository/auth_postgres.go
@@ -17,7 +17,8 @@ func NewAuthPostgres(db *sqlx.DB) *AuthPostgres {
 
 func (r *AuthPostgres) CreateUser(user notesync.User) (int, error) {
 	var id int
-	query := fmt.Sprintf(`INSERT INTO "%s" (name, username, password_hash) VALUES ($1, $2, $3) RETURNING id`, usersTable)
+	// Оберните имя таблицы в двойные кавычки, чтобы избежать конфликтов с зарезервированными словами
+	query := fmt.Sprintf(`INSERT INTO %s (name, username, password_hash) VALUES ($1, $2, $3) RETURNING id`, usersTable)
 
 	row := r.db.QueryRow(query, user.Name, user.Username, user.Password)
 	if err := row.Scan(&id); err != nil {
