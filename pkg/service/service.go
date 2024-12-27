72064f97ac56a5eb0063a4ce29bd69dfc1adebc0
My Name <rbhb0555@mail.ru>
2024-12-27 23:57:02 +0300

    install parse

--- pkg/service/service.go
+++ pkg/service/service.go
@@ -8,6 +8,7 @@ import (
 type Authorization interface {
 	CreateUser(user notesync.User) (int, error)
 	GenerateToken(username, password string) (string, error)
+	ParseToken(token string) (int, error)
 }
 
 type NotesyncList interface {
