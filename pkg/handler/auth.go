ba09f25067cccd65b44faedd5302faaddbcae964
Midiy <polyk6859@gmail.com>
2025-01-26 12:22:41 +0300

    update password users in db

--- pkg/handler/auth.go
+++ pkg/handler/auth.go
@@ -48,6 +48,30 @@ func (h *Handler) signIn(c *gin.Context) {
 	})
 }
 
+type UpdateResetPassword struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+
+func (h *Handler) UpdatePassword(c *gin.Context) {
+	var input UpdateResetPassword
+
+	if err := c.BindJSON(&input); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	_, err ;= h.service.Authorization.UpdatePassword(input.Username, input.Password)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"message":"Password update",
+	})
+}
+
 type requestResetPassword struct {
 	Email string `json:"email" binding:"required"`
 }
--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -31,6 +31,7 @@ func (h *Handler) InitRoutes() *gin.Engine {
 	{
 		auth.POST("/sign-up", h.signUp)
 		auth.POST("/sign-in", h.signIn)
+		auth.POST("/sign-UpdatePassword", h.UpdatePassword)
 		auth.POST("/request-password-reset", h.requestPasswordReset) // Новый маршрут для сброса пароля
 	}
 
