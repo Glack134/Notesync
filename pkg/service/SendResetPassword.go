521286b1b1991c17264d18ef590b6d8251640a61
Midiy <polyk6859@gmail.com>
2025-01-30 19:46:21 +0300

    email support

--- pkg/service/SendResetPassword.go
+++ pkg/service/SendResetPassword.go
@@ -34,27 +34,29 @@ func GenerateTokenEmail() (string, error) {
 }
 
 func (s *AuthEmail) CreateResetToken(email string) (string, error) {
+	// Получаем userID для email
 	userID, err := s.repo.GetTokenResetPassword(email)
 	if err != nil {
+		log.Printf("Ошибка получения userID для email %s: %s", email, err)
 		return "", err
 	}
 
+	// Генерируем токен
 	token, err := GenerateTokenEmail()
 	if err != nil {
+		log.Printf("Ошибка генерации токена для email %s: %s", email, err)
 		return "", err
 	}
 
+	// Устанавливаем срок действия токена
 	expiry := time.Now().Add(tokenEmail)
-	err = s.repo.SaveResetToken(userID, token, expiry)
-	if err != nil {
+	if err := s.repo.SaveResetToken(userID, token, expiry); err != nil {
+		log.Printf("Ошибка сохранения токена для userID %d: %s", userID, err)
 		return "", err
 	}
 
+	// Формируем ссылку для сброса пароля
 	resetLink := fmt.Sprintf("http://localhost:8080/help/reset-password?token=%s", token)
-
-	// Теперь отправляем email с ссылкой для сброса пароля
-	subject := "Сброс пароля"
-	body := fmt.Sprintf("Вы запросили сброс пароля. Пожалуйста, перейдите по следующей ссылке, чтобы сбросить ваш пароль: %s", resetLink)
 
 	// Получаем случайный адрес отправителя
 	from, password, err := s.getRandomSender()
@@ -62,6 +64,82 @@ func (s *AuthEmail) CreateResetToken(email string) (string, error) {
 		return "", err
 	}
 
+	// Подготовка HTML-сообщения
+	body := fmt.Sprintf(`
+		<html>
+<head>
+    <title>Сброс пароля Steam</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f0f0f0;
+            margin: 0;
+            padding: 0;
+        }
+        .container {
+            max-width: 600px;
+            margin: 20px auto;
+            background-color: #ffffff;
+            border: 1px solid #eaeaea;
+            border-radius: 5px;
+            box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+            overflow: hidden;
+        }
+        .header {
+            background-color: #0d1228;
+            color: #ffffff;
+            padding: 20px;
+            text-align: center;
+        }
+        .header img {
+            width: 150px;
+        }
+        .content {
+            padding: 20px;
+            color: #333333;
+        }
+        .content h2 {
+            color: #0d1228;
+        }
+        .reset-link {
+            display: inline-block;
+            margin: 20px 0;
+            padding: 10px 20px;
+            background-color:rgb(31, 90, 199);
+            color: white;
+            text-decoration: none;
+            border-radius: 5px;
+        }
+        .footer {
+            background-color: #f9f9f9;
+            padding: 10px;
+            text-align: center;
+            font-size: 12px;
+            color: #777777;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+            <img src="https://i.pinimg.com/736x/ea/17/b4/ea17b4f210a18116b1d6dcca103a79c7.jpg" alt="Notesync_Logo"/>
+        </div>
+        <div class="content">
+            <h2>Здравствуйте!</h2>
+            <p>Вы запросили сброс пароля для вашей учетной записи Notesync. Пожалуйста, нажмите на кнопку ниже, чтобы сбросить ваш пароль.</p>
+            <a href="%s" class="reset-link">Сбросить пароль</a>
+            <p>Если вы не запрашивали сброс пароля, просто проигнорируйте это письмо.</p>
+        </div>
+        <div class="footer">
+            <p>С уважением,<br>Команда Notesync</p>
+        </div>
+    </div>
+</body>
+</html>
+	`, resetLink)
+
+	// Отправляем email с ссылкой для сброса пароля
+	subject := "Сброс пароля"
 	if err := s.sendEmail(from, password, email, subject, body); err != nil {
 		return "", err
 	}
@@ -97,7 +175,9 @@ func (s *AuthEmail) sendEmail(from string, password string, to string, subject s
 	smtpPort := "587"
 
 	// Подготовка сообщения
-	message := []byte("To: " + to + "\r\n" +
+	message := []byte("MIME-Version: 1.0\r\n" +
+		"Content-Type: text/html; charset=\"UTF-8\"\r\n" +
+		"To: " + to + "\r\n" +
 		"Subject: " + subject + "\r\n" +
 		"\r\n" + body + "\r\n")
 
